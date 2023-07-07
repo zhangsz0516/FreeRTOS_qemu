805af936d2d92574bad0e981f7406eecad58cdeb
张世争 <zhangshizheng@rt-thread.com>
2023-07-07 16:17:20 +0800

    qemu debug 路径

--- CORTEX_MPS2_QEMU_IAR_GCC/qemu.sh
+++ CORTEX_MPS2_QEMU_IAR_GCC/qemu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 qemu-system-arm -machine mps2-an385 \
 	-cpu cortex-m3 \
-	-kernel ./gcc/output/RTOSDemo.out \
+	-kernel ./output/RTOSDemo.out \
 	-monitor none \
 	-nographic \
 	# -serial stdio
